983b3a4dbc7a657926095221f94bda7899bb83b3
hiro2001 <hiroyukikorea@gmail.com>
2022-01-19 15:02:21 +0900

    Add Q2

---
+++ Q2/2-1.c
@@ -0,0 +1,15 @@
+//整数値を読み込んでその値から5の和、差、積、商を表示させなさい。
+
+#include <stdio.h>
+
+int main(){
+
+	int i;
+	
+	scanf("%d", &i);
+	printf("入力された値と５の\n和は%d\n差は%d\n積は%d\n商は%d\nです。\n", i+5, i-5, i*5, i/5);
+
+	return 0;
+}
+
+
