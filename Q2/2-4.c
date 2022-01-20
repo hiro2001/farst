29e5924b5b898528c999e516701d06508f7b09de
hiro2001 <hiroyukikorea@gmail.com>
2022-01-20 11:52:46 +0900

    Add 2-5

--- Q2/2-4.c
+++ Q2/2-4.c
@@ -1,18 +1,18 @@
 //3-3.cを改変して平均を求める値の個数を可変にしなさい。
 
-#include <studio.h>
+#include <stdio.h>
 
 int main(void){
-  double i1, i2 = 0, i;
-  int n;
+  double i1, i2 = 0;
+  int i, n;
   printf("値の個数を入力してください。\n");
-  scanf("%d", &n)
-  for(i = 0; i > n; i++){
+  scanf("%d", &n);
+  for(i = 0; i < n; i++){
     scanf("%lf", &i1);
     i2 += i1;
   }
 
-  i2 /= i;
-  printf("%dつの値の平均値は%lfです。\n", i, i2);
+  i2 /= n;
+  printf("%dつの値の平均値は%.2lfです。\n", n, i2);
   return 0;
 }
---
+++ Q2/2-5.c
@@ -0,0 +1,19 @@
+//3-3.cを改変して平均を求める値の個数を配列を使用して可変にしなさい。
+
+#include <stdio.h>
+
+int main(void){
+  double i1[256];
+  int i, n;
+  printf("値の個数を入力してください。\n");
+  scanf("%d", &n);
+	i1[n] = 0;
+  for(i = 0; i < n; i++){
+    scanf("%lf", &i1[i]);
+    i1[n] += i1[i];
+  }
+
+  i1[n] /= n;
+  printf("%dつの値の平均値は%.2lfです。\n", n, i1[n]);
+  return 0;
+}
