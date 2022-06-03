9420e4feea49e8f6e39410447bba2f842e738f65
hiro2001 <hiroyukikorea@gmail.com>
2022-06-04 02:44:19 +0900

    Add

---
+++ Q3/3-6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+	int a = 0, max = 0;
+
+	printf("最小値を求めます。整数値を五つ入力してください。\n");
+	
+	for(int i = 0; i < 5; i++){
+		scanf(" %d", &a);
+		if(max < a)
+			max = a;
+	}
+
+	printf("最小値は%dです\n", max);
+	return 0;
+}
